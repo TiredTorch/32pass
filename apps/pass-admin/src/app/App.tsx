1baae72523bae7f81f1619e6cdbc829e071b8932
Viktor <viktor.ivanov@extrawest.com>
2022-09-21 08:55:22 +0300

    Created necessary libs and aps

---
+++ apps/pass-admin/src/app/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="pass-admin" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ libs/pass-user-lib/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'pass-user-lib',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/pass-user-lib',
+};
