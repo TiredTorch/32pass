20d3c5e25f55540d6678caa6df62326cd8aa9cea
Smolldo <maximqwerbich@gmail.com>
2022-09-28 23:06:06 +0300

    коммит1
    добавил проверку действия. надеюсь не сломаю все к хуям

--- apps/pass-user-e2e/src/e2e/aboutPage.cy.ts
+++ apps/pass-user-e2e/src/e2e/aboutPage.cy.ts
@@ -17,5 +17,25 @@ describe(
 				cy.get(`[data-cy="about-root"]`).contains(/want to stay in they favori/i);
 			}
 		);
+		it(
+			"Should have creators title",
+			() => {
+			  cy.get('[data-cy="creat-title"]').contains(/Creators & developers/i);
+			}
+		  );
+		  it(
+			"Should navigate back to auth form",
+			() => {
+			  cy.url().should(
+					"include",
+					"/about"
+			  );
+			  cy.get('[data-cy="unauth-header-home-button"]').click();
+			  cy.url().should(
+					"include",
+					"/login"
+			  );
+			}
+		  );
 	}
 );
